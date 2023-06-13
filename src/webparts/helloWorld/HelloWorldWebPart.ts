348728ea6eec66ffa06af98c95c5911aa8d128e5
Ana Teresa <ana.t.negri.silva@avanade.com>
2023-06-13 10:44:45 -0300

    validações atualizadas

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -8,7 +8,6 @@ import {
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 //import { escape } from '@microsoft/sp-lodash-subset';
-
 import styles from './HelloWorldWebPart.module.scss';
 import * as strings from 'HelloWorldWebPartStrings';
 import * as $ from "jquery";
@@ -38,8 +37,8 @@ export interface ISPList {
   Title: string;
   Escritor: string;
   Id: string;
-  Inicio:number;
-  Fim:number;
+  Inicio: string;
+  Fim: string;
   Avaliacao: number;
 }
 
@@ -58,16 +57,26 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
     <div hidden id="formulario">
     <h1>Realize seu cadastro:</h1>
       <form class="">
-        <label for:"title">Título:</label>
-        <input id="title" type="text" placeholder="Ex: Matéria Escura" name="title"><br>
-        <label for:"escritor">Escritor(a) do livro:</label>
-        <input id="escritor" type="text" placeholder="Ex: Blake Crouch" name="escritor"><br><br>
-        <label for:"dataInicio">Data de Início:</label>
-        <input id="dataInicio" type="date" name="dataInicio"><br>
-        <label for:"dataFim">Data de Fim:</label>
-        <input id="dataFim" type="date" name="dataFim"><br>
-        <label for:"avaliacao">Avaliação:</label>
-        <input id="avaliacao" type="number"  min="0" max="5" name="avaliacao">
+        <div>
+          <label for:"title">Título:</label>
+          <input id="title" type="text" placeholder="Ex: Matéria Escura" name="title">
+        </div>
+        <div>
+          <label for:"escritor">Escritor(a) do livro:</label>
+          <input id="escritor" type="text" placeholder="Ex: Blake Crouch" name="escritor">
+        </div>
+        <div>
+          <label for:"dataInicio">Data Inicial:</label>
+          <input id="dataInicio" type="date" name="dataInicio">
+        </div>
+        <div>
+          <label for:"dataFim">Data Final:</label>
+          <input id="dataFim" type="date" name="dataFim"><br>
+        </div>
+        <div>
+          <label for:"avaliacao">Avaliação:</label>
+          <input id="avaliacao" type="number"  min="0" max="5" name="avaliacao">
+        </div>
       </form>
       <div class="${styles.btnsCadastro}">
         <button type="submit" class="${styles.btnForm}" id="btnCadastrar">Cadastrar</button>
@@ -102,7 +111,9 @@ this.updateButton();
  }
 
  private cancelarFormulario(): void {
-  $( "#btnCancelar" ).on( "click", function() {
+  $( "#btnCancelar" ).on( "click", () => {
+    this.limparDados();
+    this.limparValidacao();
     $('#formulario').hide();
     $('#btnNovo').show();
     
@@ -134,6 +145,7 @@ this.updateButton();
       $('#btnAtualizar').show();
       $('#btnNovo').hide();
       $('#btnCadastrar').hide();
+      this.resgatarDados(this.livros[index])
     })  
   })
  }
@@ -142,6 +154,22 @@ this.updateButton();
   $("#btnAtualizar").on( "click",  () => {this._updateItem(this.itemID);});
   $('#formulario').hide();
   $('#btnNovo').show();
+}
+
+private resgatarDados(livro: ISPList): void{
+  $('input[name=title]').val(livro.Title);
+  $('input[name=escritor]').val(livro.Escritor);
+  $('input[name=dataInicio]').val(livro.Inicio.split('T')[0]);
+  $('input[name=dataFim]').val(livro.Fim.split('T')[0]);
+  $('input[name=avaliacao]').val(livro.Avaliacao);
+}
+
+private limparDados(): void{
+  $('input[name=title]').val("");
+  $('input[name=escritor]').val("");
+  $('input[name=dataInicio]').val("");
+  $('input[name=dataFim]').val("");
+  $('input[name=avaliacao]').val("");
 }
 
  private _renderListAsync(): void {
@@ -164,20 +192,129 @@ this.updateButton();
       });
   }
 
-  //post
-  private _postNewItem(): void {
+  private validateItem(): boolean {
+    let validated = true;
     const title = $('input[name=title]').val();
-    const escritor = $('input[name=escritor]').val();
-    const spOptions: ISPHttpClientOptions ={
-      "body":`{Title:'${title}', Escritor:'${escritor}' }`
+    const escritor = $('input[name=escritor]').val()
+    const dataInicio = $('input[name=dataInicio]').val();
+    const dataFim = $('input[name=dataFim]').val();
+    const avaliacao = +$('input[name=avaliacao]').val();
+
+    if(title === null || title === '') {
+      const errorMessageVisible = $(".validationTitleMessage").is(":visible");
+      if (errorMessageVisible === false) {
+            $('#title').after("<span class='validationTitleMessage' style='color:red;'>Título obrigatório.</span>");
+      }
+
+      validated = false;
     }
 
-    this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getByTitle('CadastrodeLivros')/items`, SPHttpClient.configurations.v1, spOptions)
-      .then((response: SPHttpClientResponse) => {
-        this._renderListAsync();
-      })
-      .catch(() => {console.log("post não realizado");
-      });
+    if(escritor === null || escritor === '') {
+      const errorMessageVisible = $(".validationEscritorMessage").is(":visible");
+      if (errorMessageVisible === false) {
+            $('#escritor').after("<span class='validationEscritorMessage' style='color:red;'>Escritor(a) obrigatório.</span>");
+      }
+
+      validated = false;
+    }
+
+    if( avaliacao <0 || avaliacao > 5) {
+      const errorMessageVisible = $(".validationAvaliacaoMessage").is(":visible");
+      if (errorMessageVisible === false) {
+            $('#avaliacao').after("<span class='validationAvaliacaoMessage' style='color:red;'>Avaliação de 0 a 5.</span>");
+      }
+      validated = false;
+    }
+    if(dataFim !== null && dataFim !== '') {
+      if (dataInicio === null || dataInicio === '') {
+        const errorMessageVisible = $(".validationInicioMessage").is(":visible");
+        if (errorMessageVisible === false) {
+            $('#dataInicio').after("<span class='validationInicioMessage' style='color:red;'>Preencher data início</span>");
+        }
+        validated = false;
+      }else if(moment(dataInicio).isAfter(moment(dataFim), 'day')){
+        const errorMessageVisible = $(".validationInicioMessage").is(":visible");
+        if (errorMessageVisible === false) {
+          $('#dataInicio').after("<span class='validationInicioMessage' style='color:red;'>Precisa ser antes da data final.</span>");
+        }
+        validated = false;
+      }
+
+      if(moment().isBefore(moment(dataFim), 'day')){
+        const errorMessageVisible = $(".validationFimMessage").is(":visible");
+        if (errorMessageVisible === false) {
+            $('#dataFim').after("<span class='validationFimMessage' style='color:red;'>Não pode exceder a data atual.</span>");
+        }
+        validated = false;
+      }
+      
+    }
+    return validated;
+  }
+
+  private convertBodyValues(): string {
+    const title = $('input[name=title]').val();
+    const escritor = $('input[name=escritor]').val();
+    const dataInicio = $('input[name=dataInicio]').val()
+    const dataFim = $('input[name=dataFim]').val();
+    const avaliacao = $('input[name=avaliacao]').val();
+
+    let bodyInicio = ''
+    let bodyFim = ''
+    let bodyAvaliacao = ''
+
+
+    if (dataInicio !== '' && dataInicio !== null) {
+      bodyInicio = `, Inicio:'${dataInicio}'`
+    }
+
+    if (dataFim !== '' && dataFim !== null) {
+      bodyFim = `, Fim:'${dataFim}'`
+    }
+
+    if (avaliacao !== '' && avaliacao !== null) {
+      bodyAvaliacao = `, Avaliacao:'${avaliacao}'`
+    }
+
+
+    return `{Title:'${title}', Escritor:'${escritor}'${bodyInicio}${bodyFim}${bodyAvaliacao}}`;
+  }
+
+  private limparValidacao(): void {
+    if ($(".validationTitleMessage").is(":visible")) {
+      $(".validationTitleMessage").remove();
+    }
+    if ($(".validationEscritorMessage").is(":visible")) {
+      $(".validationEscritorMessage").remove();
+    }
+    if ($(".validationAvaliacaoMessage").is(":visible")) {
+      $(".validationAvaliacaoMessage").remove();
+    }
+    if ($(".validationInicioMessage").is(":visible")) {
+      $(".validationInicioMessage").remove();
+    }
+    if ($(".validationFimMessage").is(":visible")) {
+      $(".validationFimMessage").remove();
+    }
+  }
+
+  //post
+  private _postNewItem(): void {
+    if(this.validateItem()){
+      const spOptions: ISPHttpClientOptions ={
+        "body": this.convertBodyValues()
+      }
+  
+      this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getByTitle('CadastrodeLivros')/items`, SPHttpClient.configurations.v1, spOptions)
+        .then((response: SPHttpClientResponse) => {
+          this._renderListAsync();
+          this.limparDados()
+          this.limparValidacao()
+        })
+        .catch(() => {console.log("post não realizado");
+        });
+    }
+    
   }
 
   //delete
@@ -194,20 +331,21 @@ this.updateButton();
 
   //update
   private _updateItem(Id: string): void {
-    const title = $('input[name=title]').val();
-    const escritor = $('input[name=escritor]').val();
-    const dataInicio = $('input[name=dataInicio]').val();
-    const dataFim = $('input[name=dataFim]').val();
-    const avaliacao = $('input[name=avaliacao]').val();
-    const spOptions: ISPHttpClientOptions ={
-      "body":`{Title:'${title}', Escritor:'${escritor}', Inicio:'${dataInicio}', Fim:'${dataFim}', Avaliacao:'${avaliacao}'}`,
+    if (this.validateItem()) {
+      const spOptions: ISPHttpClientOptions ={
+      "body": this.convertBodyValues(),
       "headers": {"X-HTTP-Method": "MERGE", "IF-MATCH":"*"}
     }
+
     this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('CadastrodeLivros')/items(${Id})`, SPHttpClient.configurations.v1, spOptions)
       .then((response: SPHttpClientResponse) => {
         this._renderListAsync();
+        this.limparDados();
+        this.limparValidacao()
       })
       .catch(() => {console.log("update não realizado")});
+    }
+    
   }
 
 
@@ -215,8 +353,10 @@ this.updateButton();
   private _renderList(items: ISPList[]): void {
     let tableItens = ''
     items.forEach((item: ISPList) => {
-      const dataInicio = (moment(item.Inicio)).format('DD/MM/YYYY')
-      const dataFim = (moment(item.Fim)).format('DD/MM/YYYY')
+      const dataInicio = item.Inicio !== null ? (moment(item.Inicio)).format('DD/MM/YYYY') : ''
+      const dataFim = item.Fim !== null ? (moment(item.Fim)).format('DD/MM/YYYY') : ''
+      const avaliacao = item.Avaliacao !== null ? item.Avaliacao.toString() : ''
+
       tableItens += `
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <tr class="menu-item">
@@ -225,7 +365,7 @@ this.updateButton();
         <td><span class="ms-font-l">${item.Escritor}</span></td>
         <td><span class="ms-font-l">${dataInicio}</span></td>
         <td><span class="ms-font-l">${dataFim}</span></td>
-        <td><span class="ms-font-l">${item.Avaliacao}</span></td>
+        <td><span class="ms-font-l ">${avaliacao}</span></td>
         <td>
         <span class="${styles.cursorBtn} btnExcluir material-symbols-outlined" name="action">delete_forever</span>
         <span type="submit" class="${styles.cursorBtn} btnUpdate material-symbols-outlined" name="action">edit</span>
